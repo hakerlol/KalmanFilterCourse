215e021ec77a37f7e279f1f3fac0a04000f7d0d9
UrBrain <haker-1996@mail.ru>
2018-05-07 02:14:31 +0700

    Some code optimization

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import com.Feller.Reader;
+
+public class Main {
+    public static void main(String[] args) {
+        Reader reader = new Reader("Фильтр");
+        reader.pack();
+        reader.setVisible(true);
+        reader.setSize(500, 300);
+        reader.setResizable(false);
+        reader.setLocationRelativeTo(null);
+    }
+}
